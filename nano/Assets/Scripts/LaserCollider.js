714b652be021a33468a8ecad8cc7243d2944ddc4
Mike Judge <mikelovesrobots@gmail.com>
2012-04-21 12:45:39 -0700

    Not sure what changed here.

--- nano/Assets/Scripts/LaserCollider.js
+++ nano/Assets/Scripts/LaserCollider.js
@@ -13,14 +13,14 @@ function Awake () {
 function Update () {
   var hit : RaycastHit;
   if (Physics.Raycast (transform.position, Vector3.forward, hit, maxDistance)) {
-    if (hit.transform == target) {
-      Debug.Log("target got nabbed by a laser");
-      OnTriggerRespawn.Respawn(hit.transform);
-    }
+    //if (hit.transform == target) {
+    //  Debug.Log("target got nabbed by a laser");
+    //  OnTriggerRespawn.Respawn(hit.transform);
+    //}
     
     Debug.DrawLine (transform.position, hit.point);
-    Debug.Log("Hit something " + hit.distance);
-    lineRenderer.SetPosition(1, Vector3(0, 0, hit.distance));    
+    //Debug.Log("Hit something " + hit.distance);
+    //lineRenderer.SetPosition(1, Vector3(0, 0, hit.distance));    
   }
 }
 
