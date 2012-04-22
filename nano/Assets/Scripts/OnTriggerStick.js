9bf52c1dcd84e0b89c30961921a4781b46509e1a
Mike Judge <mikelovesrobots@gmail.com>
2012-04-22 12:47:23 -0700

    Players stick to platforms now

---
+++ nano/Assets/Scripts/OnTriggerStick.js
@@ -0,0 +1,22 @@
+#pragma strict
+
+public var platform: Transform;
+private var target: Transform;
+private var lastPosition : Vector3;
+
+function Update() {
+  if (target) {
+    var positionDelta = platform.position - lastPosition;
+    Debug.Log("stick stick stick" + positionDelta);
+    target.position += positionDelta;
+  }
+  lastPosition = platform.position;
+}
+
+function OnTriggerEnter(other: Collider) {
+  target = other.gameObject.transform;
+}
+
+function OnTriggerExit(other: Collider) {
+  target = null;
+}
